e57b4cc29601c8fcb429fc6c3060204da00bf471
= <=>
2021-06-26 11:12:02 -0600

    Se sube codigo fuente del servicio productos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sonic', 900, NOW()); 
+INSERT INTO productos (nombre, precio, create_at) VALUES('LG', 850, NOW()); 
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hisense', 700, NOW()); 
+INSERT INTO productos (nombre, precio, create_at) VALUES('Xiaomi', 600, NOW()); 
+INSERT INTO productos (nombre, precio, create_at) VALUES('Huawei', 750, NOW()); 
+INSERT INTO productos (nombre, precio, create_at) VALUES('Samsung', 800, NOW()); 
